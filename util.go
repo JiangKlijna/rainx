01183a6e66e326b030f34520f2d5b3466d70660a
jiangKlijna <leil729564090@outlook.com>
2018-08-05 20:23:45 +0800

    :fire:add iphash func

--- util.go
+++ util.go
@@ -17,3 +17,20 @@ func DirExists(path string) bool {
 	}
 	return false
 }
+
+const primeRK = 16777619
+
+// Get Ip hash By Address
+func IphashByAddress(addr string) uint32 {
+	hash := uint32(0)
+	n := len(addr) - 1
+	for ; n >= 0; n-- {
+		if addr[n] == ':' {
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		hash = hash*primeRK + uint32(addr[i])
+	}
+	return hash
+}
