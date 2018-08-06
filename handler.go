955ab7a23533ae348732c7fda1334b8c36973864
jiangKlijna <leil729564090@outlook.com>
2018-08-06 18:38:34 +0800

    :fire:add ProxyHandler

--- handler.go
+++ handler.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"net/url"
 	"net/http"
+	"net/http/httputil"
 )
 
 // static
 func StaticHandler(path string) http.Handler {
 	return http.FileServer(http.Dir(path))
+}
+
+// proxy
+func ProxyHandler(path string) http.Handler {
+	targetUrl, _ := url.Parse(path)
+	return httputil.NewSingleHostReverseProxy(targetUrl)
 }
