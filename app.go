fbda541a3de73025bf2e32881265cde22cb82547
jiangKlijna <leil729564090@outlook.com>
2018-08-07 18:47:00 +0800

    :fire:add Application::Init

--- app.go
+++ app.go
@@ -4,3 +4,18 @@ type Application struct {
 	log Logger
 	set Setting
 }
+
+// Init log & set
+func (app *Application) Init() error {
+	log, err := NewLogger("access.log", L_DEBUG)
+	if err != nil {
+		return err
+	}
+	app.log = log
+	set, err := NewSetting("setting.json")
+	if err != nil {
+		return err
+	}
+	app.set = set
+	return nil
+}
